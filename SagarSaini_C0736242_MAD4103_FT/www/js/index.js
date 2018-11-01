c7a78f797550419da890ae773af55214319fdb62
sagar <sagarsaini3993@gmail.com>
2018-11-01 08:54:56 -0400

    database created

--- SagarSaini_C0736242_MAD4103_FT/www/js/index.js
+++ SagarSaini_C0736242_MAD4103_FT/www/js/index.js
@@ -1,1 +1,40 @@
-document.addEventListener()
+document.addEventListener("deviceReady",createDatabase);
+
+
+function createDatabase() {
+  db = window.openDatabase("superdb", "1.0", "My WebSQL test database", 5*1024*1024);
+  	if(!db) {
+  		// Test your DB was created
+  		alert('Your DB was not created this time');
+  		return false
+  	}
+
+
+    db.transaction(
+    		function(tx){
+    			// Execute the SQL via a usually anonymous function
+    			// tx.executeSql( SQL string, arrary of arguments, success callback function, failure callback function)
+    			// To keep it simple I've added to functions below called onSuccessExecuteSql() and onFailureExecuteSql()
+    			// to be used in the callbacks
+    			tx.executeSql(
+    				"CREATE TABLE IF NOT EXISTS heroes (name TEXT, isAvailable INTEGER)",
+    				[],
+    				onSuccessExecuteSql,
+    				onError
+    			)
+    		},
+    		onError,
+    		onReadyTransaction
+    	)
+}
+
+
+function onReadyTransaction( ){
+		console.log( 'Transaction completed' )
+	}
+	function onSuccessExecuteSql( tx, results ){
+		console.log( 'Execute SQL completed' )
+	}
+	function onError( err ){
+		console.log( err )
+	}
